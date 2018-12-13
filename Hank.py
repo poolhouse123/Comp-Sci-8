cba52860a26993101af4ab83c4e3c90c554a1896
poolhouse123 <45858369+poolhouse123@users.noreply.github.com>
2018-12-13 14:19:27 -0500

    Add files via upload

---
+++ Hank.py
@@ -0,0 +1,157 @@
+import webbrowser as wb
+import pyautogui as pg
+import time as t
+points = 0
+game = pg.prompt ("What is your favorite  game?")
+
+if game == "pubg":
+    pg.alert ("you stooge no one likes pubg")
+    points -=5
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+elif game == "fortnite":
+    pg.alert("fortnite is dying gotta play minecraft hunger games")
+    points -=5
+    wb.open("https://youtu.be/y98De45A94Y?t=12")
+elif game == "minecraft hunger games":
+    pg.alert("yee boi minecraft is cool")
+    points +=123456789123456789
+    wb.open("https://www.youtube.com/watch?v=3pRC_Yexvdg")
+elif game == "minecraft":
+    pg.alert("what is this? minecraft is the old thing now you gotta play minecraft hunger games")
+    points +=1
+    wb.open("https://www.youtube.com/watch?v=3pRC_Yexvdg")
+elif game == "roblox":
+    pg.alert("GIVE ME YOUR ROBUUUUXXXX")
+    points +=4752997328
+    wb.open("https://youtu.be/RqIVH42B_30?t=15")
+elif game == "cod":
+    pg.alert ("you stooge no one likes cod")
+    points -=5
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+else:
+    pg.alert (game + " is wierd, minecraft hunger games is where it's at")
+    wb.open("https://www.youtube.com/watch?v=Od_SaifE9Jc")
+
+t.sleep(15)
+
+place = pg.prompt ("Where is your favorite place to land in fortnite?")
+
+if place == "tilted towers":
+    pg.alert("It's called builted bowers")
+    points -=5
+    wb.open("https://youtu.be/OhScdr_xJOY?t=274")
+elif place == "builted bowers":
+    pg.alert("Finally somebody knows what it's called")
+    points *=123456789012345678928487538579023475
+    wb.open("https://youtu.be/OhScdr_xJOY?t=274")
+elif place == "haunted hills":
+    pg.alert("haunted hills is so bad")
+    points-=7
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+elif place == "where ever the llama is at":
+    pg.alert("yee kid you know how to play")
+    points +=4752997328
+    wb.open("https://www.youtube.com/watch?v=y98De45A94Y")
+elif place == "leaky lake":
+    pg.alert("yee kid you know how to play")
+    points +=4752997328
+    wb.open("https://www.youtube.com/watch?v=y98De45A94Y")
+elif place == "moisty mire":
+    pg.alert ("that is no longer a place :--------((((((((((((((((((")
+    points -=5
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+else:
+    pg.alert (str(place) + " is okay but builted bowers is better")
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+
+emote = pg.prompt ("what is your favorite emote? ")
+
+if emote == "dance moves":
+    pg.alert("dance moves is FIRE")
+    points *=2312913890489284359834547325780498
+    wb.open ("https://www.youtube.com/watch?v=xRIp5hzB6q8")
+elif emote == "default":
+    pg.alert("dance moves is FIRE")
+    points *=2312913890489284359834547325780498
+    wb.open ("https://www.youtube.com/watch?v=xRIp5hzB6q8")
+elif emote == "t pose":
+    pg.alert("DOMINANCE ASERTED")
+    points +=12345678989234908273498724897345897345897349807348952738957387538735853785373837835738753835783573587358357937328975894798324578972348957348957324957348975893427598734978
+    wb.open ("https://www.youtube.com/watch?v=sDzmXF2SQf4")
+elif emote == "hype":
+    pg.alert("it's called shute")
+    points *=5
+    wb.open("https://youtu.be/_pLMa6hs184?t=7")
+elif emote == "shute":
+    pg.alert("SHUTE IF UR COOL")
+    points *=5
+    wb.open("https://youtu.be/_pLMa6hs184?t=7")
+elif emote == "best mates":
+    pg.alert("I like that emote too")
+    points +=75
+    wb.open("https://youtu.be/ZQbRV-ropIc?t=7")
+else:
+    pg.alert(emote + " is okay but dance moves is better")
+    wb.open("https://youtu.be/Kv6Ybp1AqN0?t=9")
+
+show = pg.prompt ("What is your favorite  show?")
+
+if show == "the office":
+    pg.alert ("Good Job")
+    points -=5
+    wb.open("https://www.youtube.com/watch?v=qfNReBBUk_w")
+elif show == "family guy":
+    pg.alert("Good Job")
+    points -=5
+    wb.open("https://www.youtube.com/watch?v=LAVUFKYdJNY")
+elif show == "american dad":
+    pg.alert("good job")
+    points +=123456789123456789
+    wb.open("https://www.youtube.com/watch?v=3pRC_Yexvdg")
+elif show == "little einsteins":
+    pg.alert("we goin on a trip")
+    points +=1
+    wb.open("https://www.youtube.com/watch?v=3pRC_Yexvdg")
+elif show == "sesame street":
+    pg.alert("what up big bird")
+    points +=4752997328
+    wb.open("https://youtu.be/RqIVH42B_30?t=15")
+elif show == "keeping up with the kardashians":
+    pg.alert ("?")
+    points -=5
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+else:
+    pg.alert (show + " is wierd")
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+
+food = pg.prompt ("what is your favorite food?")
+
+if food == "cucumber":
+    pg.alert ("Good Job")
+    points -=5
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+elif food == "toothpaste":
+    pg.alert("Good Job")
+    points -=5
+    wb.open("https://youtu.be/y98De45A94Y?t=12")
+elif food == "rocks":
+    pg.alert("good job")
+    points +=123456789123456789
+    wb.open("https://www.youtube.com/watch?v=3pRC_Yexvdg")
+elif food == "pencil erasers":
+    pg.alert("we goin on a trip")
+    points +=1
+    wb.open("https://www.youtube.com/watch?v=3pRC_Yexvdg")
+elif food == "horse":
+    pg.alert("peter")
+    points +=4752997328
+    wb.open("https://youtu.be/RqIVH42B_30?t=15")
+elif food == "dogs":
+    pg.alert ("?")
+    points -=5
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+else:
+    pg.alert (food + " is wierd")
+    wb.open("https://www.youtube.com/watch?v=L0EL6JPlm3U")
+    
+pg.alert(str(points) + " is your score the record is....... alot more than that")
